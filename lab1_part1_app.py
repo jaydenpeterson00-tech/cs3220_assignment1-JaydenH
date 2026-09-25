f0b864e2b83347e4c691fcf60e3c7e9f7e108159
jaydenpeterson00-tech <jayden.peterson00@gmail.com>
2026-09-25 09:02:00 -0600

    Add files via upload

--- lab1_part1_app.py
+++ lab1_part1_app.py
@@ -3,6 +3,8 @@ import streamlit.components.v1 as components
 
 st.title("War of 5 Kings - Battle Network")
 
+st.header("Task1. Building Interactive Network of battles of the War of 5 Kings")
+
 with open("Lab1-task1-net5kings.html", "r", encoding="utf-8") as file:
     graph_html = file.read()
 
